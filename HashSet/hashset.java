fd8716afea329f31a459c165e85e86e6a0714d8f
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-09 01:18:17 +0900

    Update hashset.java

--- HashSet/hashset.java
+++ HashSet/hashset.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class hashset {
     public static void main(String[] args) {
@@ -24,26 +25,26 @@ public class hashset {
         //입력되는 값이 HashSet 내부에 존재하지 않는다면 그 값을 HashSet에 추가하고 true를 반환하고 내부에 값이 존재한다면 false를 반환합니다.
 
         //HashSet 값 삭제
-//        HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
-//        set.remove(1);//값 1 제거
-//        set.clear();//모든 값 제거
+        HashSet<Integer> setD = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
+        setD.remove(1);//값 1 제거
+        setD.clear();//모든 값 제거
         //HashSet에 값을 제거하려면 HashSet의 remove(value) 메소드를 사용하면 됩니다.
         //매개변수 value의 값이 HashSet 내부에 존재한다면 그 값을 삭제한 후 true를 반환하고 없다면 false를 반환합니다. 모든 값을 제거하려면 clear() 메소드를 사용하면 됩니다.
 
         //HashSet 크기 구하기
-//        HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
-//        System.out.println(set.size());//set 크기 : 3
-            //HashSet의 크기를 구하려면 size() 메소드를 사용하면 됩니다.
+        HashSet<Integer> setH = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
+        System.out.println(setH.size());//set 크기 : 3
+        //HashSet의 크기를 구하려면 size() 메소드를 사용하면 됩니다.
 
         //HashSet 값 출력
-//        HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
-//
-//        System.out.println(set); //전체출력 [1,2,3]
-//
-//        Iterator iter = set.iterator();	// Iterator 사용
-//        while(iter.hasNext()) {//값이 있으면 true 없으면 false
-//            System.out.println(iter.next());
-//        }
+        HashSet<Integer> setPUT = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
+
+        System.out.println(setPUT); //전체출력 [1,2,3]
+
+        Iterator iter = set.iterator();	// Iterator 사용
+        while(iter.hasNext()) {//값이 있으면 true 없으면 false
+            System.out.println(iter.next());
+        }
         //Set컬렉션을 그냥 print하게 되면 대괄호 [ ]로 묶여서 set의 전체 값이 출력됩니다.
         // Set에는 인덱스로 객체를 가져오는 get(index) 메소드가 없습니다. 대신 전체 객체를 대상으로 한 번씩 반복해서 가져오는 반복자(Iterator)를 제공합니다.
         // 반복자 이터레이터 인터페이스를 구현한 객체를 말하는데 iterator() 메소드를 호출하면 얻을 수 있습니다.
@@ -52,11 +53,10 @@ public class hashset {
 
 
         //HashSet 값 검색
-        //HashSet<Integer> set = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
-        //System.out.println(set.contains(1)); //set내부에 값 1이 있는지 check : true
+        HashSet<Integer> setS = new HashSet<Integer>(Arrays.asList(1,2,3));//HashSet생성
+        System.out.println(setS.contains(1)); //set내부에 값 1이 있는지 check : true
 
         //HashSet 내부에 원하는 값이 있는지 확인하고 싶다면 contains(value) 메소드를 사용하면 됩니다.
         //파라미터로 주어진 객체가 HashSet이 가지고 있다면 true, 아니면 false를 반환합니다.
     }
 }
-//출처 - https://coding-factory.tistory.com/554
