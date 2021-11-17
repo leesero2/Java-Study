5c3e1c60c3be7616a2fa6f56ea037dd67ed01b91
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-17 22:24:56 +0900

    첫번째 메소드 수업
    출처 - 생활코딩

---
+++ 메소드/FirstMethod.java
@@ -0,0 +1,14 @@
+public class FirstMethod {
+    public static void printTwoTimesA(){ //아래 값을 출력하는 메소드를 생성
+        System.out.println("-");
+        System.out.println("A");
+        System.out.println("A");
+    }
+    public static void main(String[] args) {
+        printTwoTimesA(); //printTwoTimesA 메소드가 실행되서 결과값이 나옴.
+
+
+        System.out.println("Hello Method");
+        System.out.println(Math.floor(1.1));//floor은 소수점을 내림해서 출력해주는 기능
+    }
+}
