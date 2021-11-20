28db3826f97f2e93a6305fe75bc2703a650bab67
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-20 20:45:01 +0900

    Create Condition2.java

---
+++ 조건문/Condition2.java
@@ -0,0 +1,13 @@
+package 자바조건문;
+
+public class Condition2 {
+    public static void main(String[] args) {
+        //if-else절은 if절의 값이 ture일때 참 조건만 출력하고 false일때 else절만 출력됨
+        if(true){
+            System.out.println(1);
+        }else{
+            System.out.println(2);
+        }
+    }
+}
+//출처 - 생활코딩
