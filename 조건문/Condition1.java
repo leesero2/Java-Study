53f6cd29ca83febf86d68a196b56f811a35f9737
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-20 20:38:05 +0900

    조건문1

---
+++ 조건문/Condition1.java
@@ -0,0 +1,21 @@
+package 자바조건문;
+
+public class Condition1 {
+    public static void main(String[] args) {
+        //if문 - if뒤의 괄호를 if절이라고 부르고 중괄호가 감싸고 있는 구간을 then절이라고 부른다.
+        //조건문에는 if절의 값이 true일때 then절이 실행된다.
+        //if절이 false이면 then절은 실행되지 않는다.
+        //if(if절){then절}
+
+        if(true){ //true이기 때문에 if조건문이 실행이 됨
+            System.out.println("result : true"); //결과값이 출력됨
+        }
+
+        if(false){ //조건이 false 이기때문에 실행이 안됨
+            System.out.println("result : ture");
+        }
+
+
+    }
+}
+//출처 - 생활코딩
