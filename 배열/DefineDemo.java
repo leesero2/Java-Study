17cf902270d49f43e14eb09f9d9f0340e2d23b53
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-22 17:16:03 +0900

    배열의 정의
    배열의 정의

---
+++ 배열/DefineDemo.java
@@ -0,0 +1,8 @@
+package 배열;
+
+public class DefineDemo {
+    public static void main(String[] args) {
+        String[] calssGroup = {"이지훈","고동현","송수현","김태원"};
+        //문자열의 데이터타입의 형식을 가진 데이터들의 모임이라는 뜻.
+    }
+}
