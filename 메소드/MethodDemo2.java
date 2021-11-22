757d41adaf19a5d427705eb1a09fd9bb040cbd23
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-22 18:13:35 +0900

    메소드를 사용하지 않는다면.
    출처 -생할코딩

---
+++ 메소드/MethodDemo2.java
@@ -0,0 +1,37 @@
+package 메소드;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        //메소드를 사용하지 않는다면 같은 코드를 계속 작성해야해서 복잡해진다.
+        //메소드를 사용하면 그 문제를 현저히 줄일 수 있다.
+        int i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        i = 0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
