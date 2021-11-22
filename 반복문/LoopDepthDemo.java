1f4b270a2260cb67459c40a999dec120b32c6fc8
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-22 16:59:31 +0900

    Create LoopDepthDemo.java
    이중 for문

---
+++ 반복문/LoopDepthDemo.java
@@ -0,0 +1,11 @@
+package 반복문;
+
+public class LoopDepthDemo {
+    public static void main(String[] args) {
+        for(int i = 0; i<10; i++){
+            for(int j = 0; j<10; j++){
+                System.out.println(i + "" + j);
+            }
+        }
+    }
+}
