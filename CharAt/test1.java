ff58946cf99c85782ffc5ba2d6afd743f4aa2851
purple_JH98 <33658056+leesero2@users.noreply.github.com>
2021-11-30 01:46:27 +0900

    charat 메소드에 대해

---
+++ CharAt/test1.java
@@ -0,0 +1,10 @@
+package CharAt;
+
+public class test1 {
+    public static void main(String[] args) {
+        String a = "banana";
+        System.out.println(a.charAt(0));
+        System.out.println(a.charAt(2));
+        System.out.println(a.charAt(5));
+    }
+}
